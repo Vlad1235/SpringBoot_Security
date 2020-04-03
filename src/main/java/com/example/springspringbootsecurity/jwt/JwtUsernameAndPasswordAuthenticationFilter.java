d392ed9f7827e511df9b17aecb0691edff4e9457
Vlad1235 <fn1235@bk.ru>
2020-04-04 04:42:05 +0900

    create token and send to client

--- src/main/java/com/example/springspringbootsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/springspringbootsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,15 +1,21 @@
 package com.example.springspringbootsecurity.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * Наследуем класс UsernamePasswordAuthenticationFilter, чтобы
@@ -33,7 +39,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
             UsernameAndPasswordAuthenticationRequest authenticationRequest =
-                    new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+                    new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class); //мапаем данные в подготовленный класс
             Authentication authentication = new UsernamePasswordAuthenticationToken(
                     authenticationRequest.getUsername(),
                     authenticationRequest.getPassword()
@@ -43,5 +49,29 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /*
+    Данный метод будет работать, только если метод attemptAuthentication успешно завершился
+    Данный метод
+    1.создаст token
+    2.отправит обратно клиенту
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        String key = "secureSecureSecureSecureSecureSecure"; // как можно длиннее и сложнее
+        String token = Jwts.builder()
+                                .setSubject(authResult.getName())
+                                .claim("authorities", authResult.getAuthorities())
+                                .setIssuedAt(new Date())
+                                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2))) // expiration time for jwt
+                                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                                .compact();
+
+       response.addHeader("Authorization","Bearer " + token); // send token back to client
     }
 }
